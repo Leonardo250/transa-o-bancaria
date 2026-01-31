e54bed729656bbb7fab5c910aa38772c7338d0dd
Leonardo250 <leomoulin2002@gmail.com>
2026-01-30 21:45:51 -0300

    projetanto a classe Usuario

---
+++ src/main/java/com/leo/transacao_simplificada/entity/Usuario.java
@@ -0,0 +1,29 @@
+package com.leo.transacao_simplificada.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "Usuario")
+@Table
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nomeCompleto;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(unique = true, nullable = false)
+    private String cpfCnpj;
+    private String senha;
+    @OneToOne(mappedBy = "usuario", cascade = CascadeType.ALL)
+    private Carteira carteira;
+}
